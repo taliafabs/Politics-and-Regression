6ac3f649c4322820cac9cd5504c745e3203847db
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-03-05 20:44:09 -0500

    download the raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,31 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the ces2020 data used in Telling Stories with Data
+# Author: Talia Fabregas
+# Date: 5 March 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
+# Any other information needed? n/a
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(dataverse)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+ces2020 <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, CC20_410, gender, educ)
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(ces2020, "data/raw_data/ces2020.csv")
+
 
          
